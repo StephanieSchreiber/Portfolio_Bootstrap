d9eb1d0d4f4d4311517ef9733c0b5dbd27b33878
Steffi <schreiber2103@gmail.com>
2019-08-29 13:33:10 -0700

    button update, circous update

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,10 +1,10 @@
-$('button span').parent().click(function () {
-    if($('button span').hasClass('glyphicon-chevron-down'))
-    {
-       $('button').html('<span class="glyphicon glyphicon-chevron-up"></span> Less Info'); 
-    }
-    else
-    {      
-        $('button').html('<span class="glyphicon glyphicon-chevron-down"></span> More Info'); 
-    }
-    }); 
+$('button_moreInfo span').parent().click(function () {
+        if($('button_moreInfo  span').hasClass('glyphicon-chevron-down'))
+        {
+           $('button_moreInfo ').html('<span class="glyphicon glyphicon-chevron-up"></span> Less Info'); 
+        }
+        else
+        {      
+            $('button_moreInfo ').html('<span class="glyphicon glyphicon-chevron-down"></span> More Info'); 
+        }
+        });
